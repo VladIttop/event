bfe6e894a004a8be6daecea250897b288608c0b6
VladIttop <129985238+VladIttop@users.noreply.github.com>
2025-02-03 18:44:51 +0000

    commit

--- src/js/partials/search.js
+++ src/js/partials/search.js
@@ -1,10 +1,13 @@
-const searchEvents =async (keyword) => {
-    const responce = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=Shhg30GRJqiVh4F5zt8WhjlFpM0sHYGU&keyword=${keyword}`)
-    const events = await responce.json()
+const searchEvents = async (keyword) => {
+  const responce = await fetch(
+    `https://app.ticketmaster.com/discovery/v2/events.json?apikey=Shhg30GRJqiVh4F5zt8WhjlFpM0sHYGU&keyword=${keyword}`
+  );
+  const events = await responce.json();
+  console.log(events);
+};
 
-    console.log(events);
-}
-const searchInput = document.querySelector(".header__input")
-searchInput.addEventListener("input", ()=>{
-    searchEvents(searchInput.value)
-})
+
+const searchInput = document.querySelector(".header__input");
+searchInput.addEventListener("input", () => {
+  searchEvents(searchInput.value);
+});
