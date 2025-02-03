b75ae6f282acc0cc0ffb4598b35629e51c478335
ksva11ly <126807129+rayytsx@users.noreply.github.com>
2025-02-03 20:26:38 +0200

    j

---
+++ src/js/search.js
@@ -0,0 +1,11 @@
+const searchEvents =async (keyword) => {
+    const responce = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=Shhg30GRJqiVh4F5zt8WhjlFpM0sHYGU&keyword=${keyword}`)
+    const events = await responce.json()
+
+    console.log(events);
+}
+const searchInput = document.querySelector(".header__input")
+searchInput.addEventListener("input", ()=>{
+    searchEvents(searchInput.value)
+})
+
