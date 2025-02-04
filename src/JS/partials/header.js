e8eeec6ac626fc1a2f5f07e01ad43eefa05ba81c
Sasha <129985153+Sasha76543@users.noreply.github.com>
2025-02-04 23:01:32 +0200

    dropdown

---
+++ src/JS/partials/header.js
@@ -0,0 +1,34 @@
+let intervalid;
+
+document.querySelectorAll('.dropdown-toggle').forkEach(e => {
+    e.addEventListener('click', e => {
+        const menu = e.currentTarget.dataset.path;
+        document.querySelectorAll('.dropdown-menu').forEach(e => {
+            if(!document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+                e.classList.remove('menu-active');
+                e.classList.remove('open');
+                document.querySelector(`[data-target=${menu}]`).classList.add('menu-active')
+                intervalid = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.add('open')                
+                }, 0)
+            }
+
+            if(document.querySelector(`[data-target=${menu}]`).classList.contains('open')) {
+                clearTimeout(intervalid)
+                document.querySelector(`[data-target=${menu}]`).classList.add('menu-active')
+                intervalid = setTimeout(() => {
+                    document.querySelector(`[data-target=${menu}]`).classList.add('open')                
+                }, 0)
+            }
+        
+            window.onclick = e => {
+                if (e.target == document.querySelector(`[data-target=${menu}]`) || e.target == document.querySelector('[data-path=${menu}]')) {
+                   return 
+                } else {
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active')
+                    document.querySelector(`[data-target=${menu}]`).classList.remove('')  
+                }
+            }
+        })
+    })
+})
