fe3812d859b5ba7f04e8aac41042fe3d3d1772c1
VladIttop <129985238+VladIttop@users.noreply.github.com>
2025-02-06 17:49:07 +0000

    commit

--- src/JS/partials/cards.js
+++ src/JS/partials/cards.js
@@ -2,6 +2,18 @@ import cardTemplate from "../../templates/cards.hbs";
 
 const API_KEY = "pxORlZn34CAbwS2qgAa40tdvGlRFZL5L";
 
+export const render = function(event) {
+  event.forEach((element) => {
+    const id = element.id;
+     const name = element.name;
+     const date = element.dates.start.localDate;
+     const place = element._embedded.venues[0].name;
+     const image = element.images[0].url;
+     const card = cardTemplate({id,name, date, image, place });
+     cardsList.innerHTML += card;
+   });
+}
+
 const cardsList = document.getElementById("cardsList");
 const getEvents = async function () {
   const response = await fetch(
@@ -9,16 +21,9 @@ const getEvents = async function () {
   );
   const data = await response.json();
   const events = data._embedded.events;
-  events.forEach((element) => {
-   const id = element.id;
-    const name = element.name;
-    const date = element.dates.start.localDate;
-    const place = element._embedded.venues[0].name;
-    const image = element.images[0].url;
-    const card = cardTemplate({id,name, date, image, place });
-    cardsList.innerHTML += card;
-  });
+  render(events);
 };
+
 
 getEvents()
   .then((data) => {
