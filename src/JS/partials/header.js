44e03991b098b19cca06841bdc1f9c5b08cf97cf
VladIttop <129985238+VladIttop@users.noreply.github.com>
2025-02-10 19:01:10 +0000

    done dropdown

--- src/JS/partials/header.js
+++ src/JS/partials/header.js
@@ -1,3 +1,5 @@
+import countries from '../countries.json';
+
 const btn = document.querySelector('.choose_btn');
 const list = document.querySelector('.dropdown-list');
 
@@ -5,25 +7,9 @@ btn.addEventListener('click', () => {
     list.classList.toggle('active')
 })
 
-const fetchCountries = async () => {
-    try {
-      const response = await fetch('./JS/countries.json'); 
-  
-      const countries = await response.json();
-  
-      renderCountries(countries);
-    } catch (error) {
-    //   console.error('Помилка завантаження:', error);
-    }
-  };
-  
-  const renderCountries = (countries) => {
-    list.innerHTML = ''; 
-    countries.forEach(({ name, code }) => {
-      const li = document.createElement('li');
-      li.textContent = `${name} (${code})`;
-      list.appendChild(li);
-    });
-  };
-  
-  fetchCountries();
+countries.forEach((country) => {
+  const li = document.createElement('li');
+  li.textContent = country.country;
+  li.classList.add('dropdown_item');
+  list.appendChild(li);
+})
