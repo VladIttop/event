f9a565ebf7db4d2f1be94af4c786686711c48473
ksva11ly <126807129+rayytsx@users.noreply.github.com>
2025-02-10 20:09:16 +0200

    .

--- src/js/partials/search.js
+++ src/js/partials/search.js
@@ -1,10 +1,11 @@
-import _ from "lodash"
+import _ from "lodash";
+import render from "./cards";
 const searchEvents = async (keyword) => {
   const responce = await fetch(
     `https://app.ticketmaster.com/discovery/v2/events.json?apikey=Shhg30GRJqiVh4F5zt8WhjlFpM0sHYGU&keyword=${keyword}`
   );
   const events = await responce.json();
-  console.log(events);
+  render(events)
 };
 
 
