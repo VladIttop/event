468cc12d7131071addce8415e22352a72b2d087b
Sasha <129985153+Sasha76543@users.noreply.github.com>
2025-02-10 20:27:56 +0200

    header

--- src/JS/partials/header.js
+++ src/JS/partials/header.js
@@ -4,3 +4,26 @@ const list = document.querySelector('.dropdown-list');
 btn.addEventListener('click', () => {
     list.classList.toggle('active')
 })
+
+const fetchCountries = async () => {
+    try {
+      const response = await fetch('./JS/countries.json'); 
+  
+      const countries = await response.json();
+  
+      renderCountries(countries);
+    } catch (error) {
+    //   console.error('Помилка завантаження:', error);
+    }
+  };
+  
+  const renderCountries = (countries) => {
+    list.innerHTML = ''; 
+    countries.forEach(({ name, code }) => {
+      const li = document.createElement('li');
+      li.textContent = `${name} (${code})`;
+      list.appendChild(li);
+    });
+  };
+  
+  fetchCountries();
