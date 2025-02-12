add12e4befa1d0a7face06a8ed3e1a61b8dcf1c2
Maxiosss <129985137+Maxiosss@users.noreply.github.com>
2025-02-12 23:06:33 +0100

    updated: modal.js

--- src/JS/partials/modal.js
+++ src/JS/partials/modal.js
@@ -1,10 +1,11 @@
 import modalTemplate from "../../templates/modal.hbs";
 import barcodeSvg from "../../images/modal/ticket.svg";
+import closeSvg from "../../images/modal/close.svg";
+
 const modalBackdrop = document.querySelector(".modal-backdrop");
 const modal = document.querySelector(".modal");
-const modalCloseBtn = document.querySelector(".modal-close__btn");
-const cards = document.querySelectorAll(".cards-item");
 const cardsList = document.querySelector(".cards-list");
+
 cardsList.addEventListener("click", (e) => {
   const card = e.target.closest("li");
   if (card) {
@@ -12,15 +13,13 @@ cardsList.addEventListener("click", (e) => {
     openModal(id, card);
   }
 });
-cards.forEach((card) => {
-  console.log(card);
-});
-console.log("hello");
+
 function openModal(id, card) {
   modal.style.display = "block";
   modalBackdrop.style.display = "block";
   modal.classList.add("is-open");
   modalBackdrop.classList.add("is-open");
+
 
   const modalImg = card.querySelector(".cards-list__image").src;
   const modalDate = card.querySelector(".cards-list__date").textContent;
@@ -34,18 +33,14 @@ function openModal(id, card) {
     modalAuthor,
   });
 }
+
 function closeModal() {
   modal.style.display = "none";
   modalBackdrop.style.display = "none";
-
   modal.classList.remove("is-open");
   modalBackdrop.classList.remove("is-open");
 }
-cards.forEach((card) => {
-  card.addEventListener("click", openModal);
-});
 
-modalCloseBtn.addEventListener("click", closeModal);
 modalBackdrop.addEventListener("click", (event) => {
   if (event.target === modalBackdrop) {
     closeModal();
@@ -61,6 +56,8 @@ async function getInfo(id, existingData) {
   const data = await response.json();
   const event = data;
   console.log(event);
+
+  const modalCloseSvg = closeSvg;
   const modalBarcodeSvg = barcodeSvg;
   const modalInfo = event.info || "No info available";
   const modalTime = event.dates.start.localTime || "Time is undefined.";
@@ -73,17 +70,22 @@ async function getInfo(id, existingData) {
   const modalPriceVip = event.priceRanges
     ? `${event.priceRanges[0].type} ${event.priceRanges[0].min} - ${event.priceRanges[0].max} ${event.priceRanges[0].currency}`
     : "VIP price is undefined";
+  const moreFromAuthor = event.url;
 
   const modalData = {
     ...existingData,
+    modalCloseSvg,
     modalInfo,
     modalTime,
     modalCity,
     modalPrice,
     modalPriceVip,
     modalBarcodeSvg,
+    moreFromAuthor,
   };
 
   const modalHTML = modalTemplate(modalData);
-  modal.innerHTML = modalHTML;
+  modal.innerHTML = modalHTML; 
+  const modalCloseBtn = modal.querySelector(".modal-close__btn");
+  modalCloseBtn.addEventListener("click", closeModal);
 }
