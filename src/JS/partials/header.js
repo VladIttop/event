5d6b5320e1ab41decbf3f3d703fdcf8d49cc684b
VladIttop <129985238+VladIttop@users.noreply.github.com>
2025-02-13 18:55:47 +0000

    .

--- src/JS/partials/header.js
+++ src/JS/partials/header.js
@@ -1,4 +1,5 @@
 import countries from "../countries.json";
+import render from "./cards";
 
 const btn = document.querySelector(".choose_btn");
 const list = document.querySelector(".dropdown-list");
@@ -15,18 +16,23 @@ countries.forEach((country) => {
   li.classList.add("dropdown_item");
   li.setAttribute("data-id", code);
   list.appendChild(li);
+  li.addEventListener("click", () => {
+    const country = li.getAttribute("data-id");
+    fetchCountries(country);
+  });
 });
 
 const fetchCountries = async function (countryCode) {
   try {
     const response = await fetch(
-      "https://app.ticketmaster.com/discovery/v2/events.json?apikey=pxORlZn34CAbwS2qgAa40tdvGlRFZL5L"
+      `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${countryCode}&apikey=pxORlZn34CAbwS2qgAa40tdvGlRFZL5L`
     );
-    const data = await response.json()
-    console.log(data)
-    const embeded = data._embedded.events;
-    console.log(embeded)
-    // const countries = embeded.map(country => )
-  } catch (error) {}
+    const data = await response.json();
+    const events = data._embedded.events;
+    render(events);
+  } catch (error) {
+    const cardsList = document.getElementById("cardsList");
+    cardsList.textContent = "No Info Found";
+    
+  }
 };
-fetchCountries()
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./JS/partials/cards";
-import "./JS/partials/header"
+import "./JS/partials/header.js"
 import "./JS/partials/cards"
 import "./JS/partials/search";
 import "./JS/partials/modal.js";
